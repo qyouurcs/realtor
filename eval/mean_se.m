fe3a8cece1e18c3b9ea3127c4d0af5b474e6a46c
Quanzeng You <qyou@cs.rochester.edu>
2015-05-24 11:20:02 -0400

    update eval

--- eval/mean_se.m
+++ eval/mean_se.m
@@ -1,11 +1,15 @@
-fns = dir('./cut_*pred.txt');
+function mean_se(root_dir)
+
+fns = dir([ root_dir '/' 'cut_*pred.txt']);
 
 for i = 1:numel(fns)
     fn = fns(i).name;
-    data = load(fn);
-    mse = sum((data(:,3) - data(:,2)).^2) / size(data,1);
+    data = load(fullfile(root_dir,fn));
+    mse = sum((data(:,4) - data(:,2)).^2) / size(data,1);
     rmse = sqrt(mse);
-    %fprintf('%s, %f, %f\n', fns(i).name, mse, rmse);
-    fprintf('%f, %f\n', mse, rmse);
+    fprintf('%s, %f, %f\n', fns(i).name, mse, rmse);
+    %fprintf('%f, %f\n', mse, rmse);
     
+end
+
 end
