ba69a8c38ca2c91102a034cc143580e595af67a0
Quanzeng You <qyou@cs.rochester.edu>
2015-05-26 09:02:27 -0400

    add some shell script

--- LSTM_realtor_sep_san_jose.sh
+++ LSTM_realtor_sep_san_jose.sh
@@ -12,14 +12,22 @@ area_code=`ls $post_sep`
 param_s=`echo "$param" | awk '{gsub(" +", "-", $0); gsub("/", "_", $0); print $0;}'`
 conf=`echo ${conf_fn} | awk -F\. '{print $1}'`
 
-idx=0
+area_cnt=`ls $post_sep | wc -l`
+
+idx=1
 for code in $area_code
 do
     gpu=`echo $idx | awk '{if( $0 % 2) print 1; else print 0}'`
     code_dir=$post_sep/$code
     val_fn=$val_dir/$code/batch-0.npz
-    cmd="\"THEANO_FLAGS=device=gpu"$gpu\"" python LSTM_realtor_sep.py "$conf_fn" "$code_dir" "$val_fn" > "${log_dir}/${conf}_${code}_${param_s}".log"
-    THEANO_FLAGS="device=gpu$gpu" python LSTM_realtor_sep.py $conf_fn $code_dir $val_fn > ${log_dir}/${conf}_${code}_${param_s}.log &
+    cmd="\"THEANO_FLAGS=device=gpu"$gpu\"" python LSTM_realtor_sep.py "$conf_fn" "$code_dir" "$val_fn" $param > "${log_dir}/${conf}_${code}_${param_s}".log"
+    #THEANO_FLAGS="device=gpu$gpu" python LSTM_realtor_sep.py $conf_fn $code_dir $val_fn $param > ${log_dir}/${conf}_${code}_${param_s}.log &
     echo $cmd
     idx=`echo $idx" + 1" | bc`
+    
+    if [ $idx -eq $area_cnt ]; then
+        THEANO_FLAGS="device=gpu$gpu" python LSTM_realtor_sep.py $conf_fn $code_dir $val_fn $param > ${log_dir}/${conf}_${code}_${param_s}.log 
+    else
+        THEANO_FLAGS="device=gpu$gpu" python LSTM_realtor_sep.py $conf_fn $code_dir $val_fn $param > ${log_dir}/${conf}_${code}_${param_s}.log &
+    fi
 done
---
+++ run_scrip.sh
@@ -0,0 +1,3 @@
+sh LSTM_realtor_sep_san_jose.sh
+
+sh LSTM_realtor_sep_san_jose2.sh
