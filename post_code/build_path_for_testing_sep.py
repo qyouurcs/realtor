2893426a3a4bcbc0bddbcac8e683ce273d9429a5
Quanzeng You <qyou@cs.rochester.edu>
2015-05-27 17:02:41 -0400

    add data

--- post_code/build_path_for_testing_sep.py
+++ post_code/build_path_for_testing_sep.py
@@ -8,16 +8,20 @@ import datetime
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 4:
-        print 'Usage: {0} <sim_fn> <train_id> <val_id> [num_path_per_node=2] [len_p=10]'.format(sys.argv[0])
+    if len(sys.argv) < 5:
+        print 'Usage: {0} <sim_fn> <train_id> <val_id> <save_dir> [num_path_per_node=3] [len_p=10]'.format(sys.argv[0])
         sys.exit()
 
-    num_p_node = 2
+    num_p_node = 3
     len_p = 10
-    if len(sys.argv) >= 5:
-        num_p_node = int(sys.argv[4])
+    save_dir = sys.argv[4]
+    
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
     if len(sys.argv) >= 6:
-        len_p = int(sys.argv[5])
+        num_p_node = int(sys.argv[5])
+    if len(sys.argv) >= 7:
+        len_p = int(sys.argv[6])
 
     dict_id_code = {}
     idx = 0
@@ -27,7 +31,6 @@ if __name__ == '__main__':
             parts = aline.split()
             dict_id_code[idx] = int(parts[-1])
             idx += 1
-
     dict_val = {}
 
     with open(sys.argv[3],'r') as fid:
@@ -57,7 +60,12 @@ if __name__ == '__main__':
     ts = ts.replace(' ','_')
     ts = ts.replace(':','_')
     save_fn = os.path.splitext(os.path.basename(sys.argv[1]))[0] + '_testing-path-{0}-{1}-{2}.txt'.format(num_p_node,len_p, ts)
+    save_fn = os.path.join(save_dir, save_fn)
+
     fid = open(save_fn,'wb')
+    if len(dict_graph) > 1:
+        print "Error, should not happen"
+        sys.exit()
 
     for code in dict_graph:
         edge_list = dict_graph[code]
