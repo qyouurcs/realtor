5019221cf8c60bad3e433068d87758edbd158db3
Quanzeng You <qyou@cs.rochester.edu>
2015-05-28 16:02:31 -0400

    add pred_sep

---
+++ pred_sep/cut_txt.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+    echo $0" <base_dir>"
+    exit
+fi
+
+base_dir=$1
+
+txts=`ls ${base_dir}/*pred.txt`
+
+
+for txt in $txts
+do
+    
+    base_name=`basename $txt`
+    cut -d" " -f1,2,3,4 $txt > ${base_dir}/cut_$base_name
+done
