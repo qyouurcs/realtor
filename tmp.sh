e3a76dded44495bef76dac215c8cce3d0b4bc792
Quanzeng You <qyou@cs.rochester.edu>
2015-05-29 01:39:29 -0400

    updated for avg

--- tmp.sh
+++ tmp.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-if [[ ! $1 ]]; then
-        echo -e "Usage:\n\n\t$0 'file name'"
-            exit 1
-        fi
-
-        f=$(ls 2>/dev/null -l /proc/*/fd/* | fgrep "$1 (deleted" | awk '{print $9}')
-
-        if [[ $f ]]; then
-                echo "fd $f found..."
-                    cp -v "$f" "$1"
-                else
-                        echo >&2 "No fd found..."
-                            exit 2
-                        fi
+sh  LSTM_realtor_sep_san_jose_200_100_avg.sh
+sh  LSTM_realtor_sep_san_jose_300_100_avg.sh
+sh  LSTM_realtor_sep_san_jose_100_100_avg.sh
